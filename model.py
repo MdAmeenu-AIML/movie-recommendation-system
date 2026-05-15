b9e473ca62504ac3e4b0f5ccd3426b7a293da977
Ai-vth-Ameenu <mohammedameenulaman01@gmail.com>
2026-05-15 10:40:51 +0530

    Refactor movie recommendation logic and dataset
    Updated dataset to 'tmdb_5000_movies.csv' and refined movie search functionality to include an overview display.

--- model.py
+++ model.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv("movies.csv")
+df = pd.read_csv("tmdb_5000_movies.csv")
+
+# Keep useful columns
+df = df[["title", "overview"]]
+
+# Remove missing values
+df = df.dropna()
 
 # User input
 movie = input("Enter a movie name: ")
 
 # Find movie
-result = df[df["movie"].str.lower() == movie.lower()]
+result = df[df["title"].str.lower() == movie.lower()]
 
 if not result.empty:
 
-    print("\nRecommended Movies:")
+    print("\nMovie Found!")
+    print("\nOverview:\n")
 
-    print("-", result.iloc[0]["recommendation1"])
-    print("-", result.iloc[0]["recommendation2"])
-    print("-", result.iloc[0]["recommendation3"])
+    print(result.iloc[0]["overview"])
 
 else:
     print("Movie not found")
